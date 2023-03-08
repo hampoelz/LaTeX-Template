492c44dedfd77e0751f228a18c49d6d3eba52ad3
Rene <rene_git@hampoelz.net>
2023-03-08 09:50:29 +0000

    chore(install): :children_crossing: Option to force remove MikTeX

--- scripts/install.bat
+++ scripts/install.bat
@@ -92,8 +92,12 @@ if not "%~n0" == "install" (
 exit
 
 :check_miktex
+    set /a check_miktex_count=0
+    call:uninstall_miktex
+
+:uninstall_miktex
     call miktex --help >nul 2>&1 || goto:EOF
-    
+
     cls
     echo.
     echo ========================================================
@@ -105,6 +109,19 @@ exit
     echo If you are forced to use MikTeX you have to install the
     echo required software manually. More information can be found
     echo in the wiki.
+
+    if %check_miktex_count% lss 3 (
+        set /a check_miktex_count+=1
+        call:soft_uninstall_miktex
+    ) else (
+        call:force_uninstall_miktex
+    )
+
+    call:refresh_env
+    goto:uninstall_miktex
+    goto:EOF
+
+:soft_uninstall_miktex
     echo.
     echo The script will open a window listing all your installed
     echo software - search for MikTeX and uninstall it to proceed.
@@ -116,9 +133,22 @@ exit
     echo After MikTeX has been uninstalled press any key to continue.
     echo.
     pause
+    goto:EOF
 
-    call:refresh_env
-    goto:check_miktex
+:force_uninstall_miktex
+    echo.
+    echo Apparently the uninstall did not work,
+    echo do you want to force remove miktex?
+    echo.
+
+    echo.
+    choice /c YN /m "Force remove MikTeX?"
+    echo.
+    if %errorlevel% equ 2 goto:EOF
+
+    for /f "usebackq delims=" %%i in (`"where miktex"`) do (
+        rmdir /s /q "..\..\..\%%i
+    )
     goto:EOF
 
 :setup_vscode
